122872e53ba2264c2c54132a52d6e245e2ab2b39
denismahoff <bogmyti88@mail.ru>
2023-05-01 12:56:10 +0400

    change name test

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_add_to_basket_button_languages(browser):
+def test_add_to_basket_button(browser):
     browser.get(link)
     time.sleep(0)
 
