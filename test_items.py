6d366bbfd751fad9150e3b88728540b2bc19f1db
denismahoff <bogmyti88@mail.ru>
2023-05-01 12:54:53 +0400

    edit assert

--- test_items.py
+++ test_items.py
@@ -7,9 +7,10 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_add_to_basket_button_languages(browser):
     browser.get(link)
     time.sleep(0)
-    button = browser.find_element(By.CSS_SELECTOR,
-                                  "#add_to_basket_form > button"
-                                  ).text
+
+    button = browser.find_elements(By.CSS_SELECTOR,
+                                   "#add_to_basket_form > button"
+                                   )
 
     assert button, "Button has not been found."
-    print('Button is name:' + button)
+    # print('Button is name:' + button)
